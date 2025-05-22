39408a6369ff2b01c8d59337ceea11d9c5e62f68
Shijilp8484 <msp97279@gmail.com>
2025-05-23 00:55:26 +0530

    Vowel or not

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char arr[100];
-    int freq[26] = {0};  
-    printf("Enter a string: ");
-    scanf("%s", arr);  
-    
-    for (int i = 0; arr[i] != '\0'; i++) {
-        if (arr[i] >= 'a' && arr[i] <= 'z') {
-            freq[arr[i] - 'a']++;
-        }
+
+int main()
+{
+int n,i,count=0,cons=0;
+char arr[50];
+printf("enter string :");
+scanf("%s",arr);
+
+int length=strlen(arr);
+for(i=0;i<length;i++){
+    if(arr[i]=='a' ||  arr[i]=='e'  ||  arr[i]=='i'  ||   arr[i]=='u'){
+        count++;
     }
-
-
-    printf(" Frequency:\n");
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] > 0) {
-            printf("%c = %d\n", i + 'a', freq[i]);
-        }
+    else{
+        cons++;
     }
-
-    return 0;
+   
+        
+    }
+     printf("  vowel  %d\n",count);
+    printf(" consonent  %d\n" ,cons);
 }
+
+
+    
