a2fcc77cf952e116777d89869c38684886f9c36d
Shijilp8484 <msp97279@gmail.com>
2025-05-23 00:51:55 +0530

    Add files via upload
    String frequency

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-   int i,number;
-   printf("enter a number\n");
-   scanf("%d",&number);
-   for(i=0;i<=10;i++){
-       printf("%d\n",i*number);
-   }
-   return 0;
+int main() {
+    char arr[100];
+    int freq[26] = {0};  
+    printf("Enter a string: ");
+    scanf("%s", arr);  
+    
+    for (int i = 0; arr[i] != '\0'; i++) {
+        if (arr[i] >= 'a' && arr[i] <= 'z') {
+            freq[arr[i] - 'a']++;
+        }
+    }
+
+
+    printf(" Frequency:\n");
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            printf("%c = %d\n", i + 'a', freq[i]);
+        }
+    }
+
+    return 0;
 }
