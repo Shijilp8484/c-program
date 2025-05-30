9e98cefd01d29f837ce55a228fd549399846c06d
Shijilp8484 <msp97279@gmail.com>
2025-05-30 12:07:05 +0530

    Add files via upload

---
+++ switch-day.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+  
+  	// Day number
+    int day = 2;
+    
+  	// Switch statement to print the name of the day
+  	// on the basis of day number
+    switch (day) {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+    default:
+        printf("Invalid Input");
+        break;
+    }
+  
+    return 0;
+}
