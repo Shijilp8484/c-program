fc9e80f8c32ae25f451040672712abf6a357380c
Shijilp8484 <msp97279@gmail.com>
2025-05-30 12:05:04 +0530

    Add files via upload

---
+++ equal or not.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1=2;
+    int num2=2;
+    if(num1==num2)
+    {
+        printf("number is equal");
+        
+    }
+        else {
+        printf("number are not equal");
+        
+    }
+
+
+
+    return 0;
+}
